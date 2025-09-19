6ada66364cb75fe6e4055806dc082a9c49fac476
Sarvagya Yadav <sarvagyaydav26@gmail.com>
2025-09-19 14:02:58 +0530

    to check if changes from VS code reflect in Git hub

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ with open('credit_risk_model.pkl', 'rb') as file:
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])   # Will only accept POST requests.
 def predict():
     try:
-        data = request.get_json(force=True)
+        data = request.get_json(force=True)         # read JSON from request
         print("✅ Received:", data)
         features = np.array(data['features']).reshape(1, -1)    #Converts the array from 1D to 2D → required shape for prediction.
         prediction = model.predict(features)[0]
@@ -25,3 +25,5 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use dynamic port from Render
     app.run(host="0.0.0.0", port=port)
+    
+#test
